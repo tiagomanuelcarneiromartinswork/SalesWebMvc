4c0401839be612b225cfdc0b61913ef24fd06b90
Tiago Manuel Martins <tiago.m.martins@criticalsoftware.com>
2020-05-25 17:12:35 +0100

    Crud Scaffolding

---
+++ SalesWebMvcUdemyFinalProject/Data/SalesWebMvcUdemyFinalProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcUdemyFinalProject.Models;
+
+namespace SalesWebMvcUdemyFinalProject.Data
+{
+    public class SalesWebMvcUdemyFinalProjectContext : DbContext
+    {
+        public SalesWebMvcUdemyFinalProjectContext (DbContextOptions<SalesWebMvcUdemyFinalProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcUdemyFinalProject.Models.Department> Department { get; set; }
+    }
+}
--- SalesWebMvcUdemyFinalProject/Startup.cs
+++ SalesWebMvcUdemyFinalProject/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcUdemyFinalProject.Data;
 
 namespace SalesWebMvcUdemyFinalProject
 {
@@ -33,6 +35,9 @@ namespace SalesWebMvcUdemyFinalProject
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvcUdemyFinalProjectContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcUdemyFinalProjectContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
