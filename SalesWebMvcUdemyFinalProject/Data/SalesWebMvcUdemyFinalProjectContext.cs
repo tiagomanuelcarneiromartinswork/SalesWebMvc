37589348c39d8d0b02e7515b734735f2b7b1de9f
Tiago Manuel Martins <tiago.m.martins@criticalsoftware.com>
2020-05-26 12:21:30 +0100

    Other entities and second migration

--- SalesWebMvcUdemyFinalProject/Data/SalesWebMvcUdemyFinalProjectContext.cs
+++ SalesWebMvcUdemyFinalProject/Data/SalesWebMvcUdemyFinalProjectContext.cs
@@ -14,6 +14,10 @@ namespace SalesWebMvcUdemyFinalProject.Data
         {
         }
 
-        public DbSet<SalesWebMvcUdemyFinalProject.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
+
     }
 }
--- SalesWebMvcUdemyFinalProject/Models/Department.cs
+++ SalesWebMvcUdemyFinalProject/Models/Department.cs
@@ -1,8 +1,36 @@
-﻿namespace SalesWebMvcUdemyFinalProject.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvcUdemyFinalProject.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller s)
+        {
+            Sellers.Add(s);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(s => s.TotalSales(initial, final));
+        }
+
+
     }
 }
